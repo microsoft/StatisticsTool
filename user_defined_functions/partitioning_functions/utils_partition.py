19141029d1ecfe62896a8dca3a246e136a4074eb
Darina Bal <darinabal@microsoft.com>
2023-05-11 17:25:12 +0300

    movedc score partition to partition utils and implemented it for general case. rename face partition to face_bb_size_partiotion to call the bb partition function from general utils

--- user_defined_functions/partitioning_functions/utils_partition.py
+++ user_defined_functions/partitioning_functions/utils_partition.py
@@ -11,8 +11,7 @@ def bb_size_partition(dataframe, from_file=False, img_w_h=(1920,1080), bb_size_t
 
     type_ = float
     bb_size_gt = dataframe['width_gt'].values.astype(type_) * dataframe['height_gt'].values.astype(type_)
-    # replace nan with zero
-    # bb_size_gt = np.nan_to_num(bb_size_gt)
+
     bb_size_gt[np.isnan(bb_size_gt)] = 0
     gt_exists = dataframe['detection_gt'].notnull().values.astype('bool')
 
@@ -21,20 +20,29 @@ def bb_size_partition(dataframe, from_file=False, img_w_h=(1920,1080), bb_size_t
     bb_key_medium = (bb_size_gt > bb_size_small ) & (bb_size_gt <= bb_size_medium) & gt_exists
     bb_key_large  = (bb_size_gt > bb_size_medium) & gt_exists
 
-    # cond_tiny_less   = (bb_size_gt <= bb_size_tiny)
-    # cond_tiny_more   = (bb_size_gt >  bb_size_tiny)
-    # cond_small_less  = (bb_size_gt <= bb_size_small)
-    # cond_small_more  = (bb_size_gt >  bb_size_small)
-    # cond_medium_less = (bb_size_gt <= bb_size_medium)
-    # cond_medium_more = (bb_size_gt >  bb_size_medium)
-    # bb_key_tiny   = cond_tiny_less & gt_exists
-    # bb_key_small  = cond_tiny_more & cond_small_less & gt_exists
-    # bb_key_medium = cond_small_more & cond_medium_less & gt_exists
-    # bb_key_large  = cond_medium_more & gt_exists
-
     bb_size = {'possible partitions': ['tiny', 'small', 'medium', 'large'], 
                'masks': [bb_key_tiny, bb_key_small, bb_key_medium, bb_key_large]}
 
     desired_masks = {'Bounding box size': bb_size}
+
+    return desired_masks
+
+def score_partition(dataframe, from_file=False, score_ranges=(0.25, 0.5, 0.75)):
+    type_ = float if from_file else object
+    score = dataframe['Score'].values.astype(type_)
+
+    score_ranges = (0,) + score_ranges + (1,)
+
+    poss_part_names = []
+    masks_list = []
+
+    for i in range(len(score_ranges)-1):
+        curr_mask = (score > score_ranges[i]) & (score <= score_ranges[i+1])
+        masks_list.append(curr_mask)
+        poss_part_names.append('>' + str(score_ranges[i]) + ' and <=' + str(score_ranges[i+1]))
+
+    score = {'possible partitions': poss_part_names, 'masks': masks_list}
+
+    desired_masks = {'Score': score}
 
     return desired_masks
