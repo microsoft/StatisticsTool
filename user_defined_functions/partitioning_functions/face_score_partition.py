7f00e432a745b397d2597adb57847c474ee676bd
Darina Bal <darinabal@microsoft.com>
2023-05-11 17:27:59 +0300

    add face score partition

---
+++ user_defined_functions/partitioning_functions/face_score_partition.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os, sys
+current_file_directory = os.path.realpath(__file__)
+# adding the statistics_tool folder to path
+sys.path.append(os.path.join(current_file_directory, '..'))
+from utils_partition import score_partition
+
+def face_bb_size_partition(dataframe, from_file=False):
+
+    desired_masks = score_partition(dataframe, from_file=from_file, score_ranges=(0.25, 0.5, 0.75))
+
+    return desired_masks
