9d636628aba6988eb5139d694eeb43829774cb1d
Gal Gozes <v-galgozes@microsoft.com>
2023-05-16 14:48:10 +0300

    added edges

--- user_defined_functions/partitioning_functions/size_horizontal_vertical_and_bbox_meta.py
+++ user_defined_functions/partitioning_functions/size_horizontal_vertical_and_bbox_meta.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width=1920, img_height=1080):
     type_ = float if from_file else object
@@ -21,22 +22,28 @@ def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width
     prd_x_center = (2*prd_x + prd_width)/2
     label_x_center = (2*label_x + label_width)/2
 
-
     # masking by the area of the bounding box
-    prd_large_mask = prd_width + prd_height >= (img_width + img_height)/1.3
-    prd_medium_mask = (prd_width + prd_height < (img_width + img_height)/1.3) & (prd_width + prd_height >= (img_width + img_height)/3)
-    prd_small_mask = np.logical_not(prd_large_mask | prd_medium_mask)
+    
+    tiny_object_pct = 0.052  # 100px @1920
+    small_object_pct = 0.15625  # 300px @1920
+    large_object_pct = 0.3125  # 600px @1920
 
-    label_large_mask = label_width + label_height >= (img_width + img_height)/1.3
-    label_medium_mask = (label_width + label_height < (img_width + img_height)/1.3) & (label_width + label_height >= (img_width + img_height)/3)
-    label_small_mask = np.logical_not(label_large_mask | label_medium_mask)
+    gt_exists = dataframe['detection_gt'].notnull().values.astype('bool')
+    tiny_obejcts = ((label_width <= (tiny_object_pct * img_width)) & gt_exists) | (
+        (prd_width < (tiny_object_pct * img_width)) & np.logical_not(gt_exists))
+    small_obejcts = (((label_width < (small_object_pct * img_width)) & (label_width > (tiny_object_pct * img_width))) & gt_exists) | (
+        ((prd_width < (small_object_pct * img_width)) & (prd_width > (tiny_object_pct * img_width))) & np.logical_not(gt_exists))
+    large_obejcts = ((label_width > (large_object_pct * img_width)) & gt_exists) | (
+        (prd_width > (large_object_pct * img_width)) & np.logical_not(gt_exists))
+    medium_objects = np.logical_not(
+        (tiny_obejcts | small_obejcts | large_obejcts))
 
-    large_mask = prd_large_mask | (label_large_mask & dataframe['x'].isnull())
-    small_mask = prd_small_mask | (label_small_mask & dataframe['x'].isnull())
-    medium_mask = prd_medium_mask | (label_medium_mask & dataframe['x'].isnull())
-
-
-    size = {'possible partitions': ['large','medium', 'small'], 'masks': [large_mask, medium_mask, small_mask]}
+    size = {'possible partitions': [
+        'large', 'medium', 'small', 'tiny'], 'masks': [large_obejcts, medium_objects, small_obejcts, tiny_obejcts]}
+    is_tiny = {'possible partitions': [
+        'valid_width', 'tiny_width'], 'masks': [np.logical_not(tiny_obejcts), tiny_obejcts]}
+    valid_size = {'possible partitions': [
+        'v_large', 'v_medium', 'v_small'], 'masks': [large_obejcts, medium_objects, small_obejcts]}
 
     # masking by x value
     prd_left_x = prd_x_center < img_width/2
@@ -55,8 +62,28 @@ def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width
     lower_mask = prd_lower_y | (label_lower_y & dataframe['x'].isnull())
     upper_mask = prd_upper_y | (label_upper_y & dataframe['x'].isnull())
     y_position = {'possible partitions': ['up', 'down'], 'masks': [upper_mask, lower_mask]}
+    
+    desired_masks = {'size': size, 'is_tiny': is_tiny, 'valid_size': valid_size, 'x position': x_position, 'y position': y_position}
 
-    desired_masks = {'size': size, 'x position': x_position, 'y position': y_position}
+    # masking by iou
+    bins = [-1, 0, 0.25, 0.5, 0.75, 1.1]
+    labels = ['0', '0-0.25', '0.25-0.5', '0.5-0.75', '0.75-1']
+
+    dataframe['iou_binned'] = pd.cut(
+        dataframe['iou'], bins=bins, labels=labels)
+    dataframe['iou_binned'] = dataframe['iou_binned'].values.add_categories(
+        'missing iou')
+    dataframe['iou_binned'].fillna('missing iou', inplace=True)
+    labels = ['missing iou'] + labels
+    masks = []
+    for label in labels:
+        curr_mask = (dataframe['iou_binned'] == label)
+        masks.append(curr_mask)
+
+    iou = {'possible partitions': labels[::-1], 'masks': masks[::-1]}
+    desired_masks_iou = {'iou': iou}
+
+    desired_masks.update(desired_masks_iou)
 
 
     # masking by bbox proximity to x axis edge
@@ -64,7 +91,7 @@ def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width
     prd_edge_mask = (prd_x < fifth_width)| (prd_x2 > img_width - fifth_width)
     label_edge_mask = (label_x < fifth_width) | (label_x2 > img_width - fifth_width)
     edge_mask = prd_edge_mask | (label_edge_mask & dataframe['x'].isnull())
-    edge_mask_dict = {'possible partitions': ['edgeX', 'not edgeX'], 'masks': [edge_mask, np.logical_not(edge_mask)]}
+    edge_mask_dict = {'possible partitions': ['centerX', 'edgeX'], 'masks': [np.logical_not(edge_mask), edge_mask]}
     desired_masks.update({"isEdgeX": edge_mask_dict})
 
     # masking by bbox center proximity to x axis edge
@@ -73,7 +100,7 @@ def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width
     label_left_x = label_x_center < img_width / 4
     label_right_x = label_x_center > img_width - img_width / 4
     centerBB_edgeX_mask = prd_right_x | prd_left_x | (label_right_x & dataframe['x'].isnull()) | (label_left_x & dataframe['x'].isnull())
-    centerBB_edgeX_mask_dict = {'possible partitions': ['centerBB_EdgeX', 'not centerBB_EdgeX'], 'masks': [centerBB_edgeX_mask, np.logical_not(centerBB_edgeX_mask)]}
+    centerBB_edgeX_mask_dict = {'possible partitions': ['centerBB_CenterX', 'centerBB_EdgeX'], 'masks': [np.logical_not(centerBB_edgeX_mask), centerBB_edgeX_mask]}
     desired_masks.update({"isCenterEdgeX": centerBB_edgeX_mask_dict})
     
     # masking by bbox proximity to x axis edge
@@ -81,8 +108,51 @@ def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width
     prd_edge_mask = (prd_y < fifth_height) | (prd_y2 > img_height - fifth_height)
     label_edge_mask = (label_y < fifth_height) | (label_y2 > img_height - fifth_height)
     edge_mask = prd_edge_mask | (label_edge_mask & dataframe['x'].isnull())
-    edge_mask_dict = {'possible partitions': ['edgeY', 'not edgeY'], 'masks': [edge_mask, np.logical_not(edge_mask)]}
+    edge_mask_dict = {'possible partitions': ['edgeY', 'CenterY'], 'masks': [edge_mask, np.logical_not(edge_mask)]}
     desired_masks.update({"isEdgeY": edge_mask_dict})
+
+
+    # person on edges
+    # ---------------
+    elongation_ratio_medium = 2
+    elongation_ratio_high = 3
+    margin_px = 20
+
+    gt_bb_on_boundary = ((dataframe['x_gt'] < 0 + margin_px) | (
+        dataframe['x_gt'] + dataframe['width_gt'] > img_width - margin_px))
+    prd_bb_on_boundary = ((dataframe['x'] < 0 + margin_px) | (
+        dataframe['x'] + dataframe['width'] > img_width - margin_px))
+
+    gt_high_elongation = (
+        (dataframe['height_gt'] / dataframe['width_gt']) > elongation_ratio_high) & gt_bb_on_boundary
+    gt_medium_elongation = ((dataframe['height_gt'] / dataframe['width_gt']) > elongation_ratio_medium) & (
+        (dataframe['height_gt'] / dataframe['width_gt']) <= elongation_ratio_high) & gt_bb_on_boundary
+    gt_low_elongation = np.logical_not(
+        (gt_high_elongation | gt_medium_elongation)) & gt_bb_on_boundary
+    gt_inside = np.logical_not(gt_bb_on_boundary)
+
+    prd_high_elongation = (
+        (dataframe['height'] / dataframe['width']) > elongation_ratio_high) & prd_bb_on_boundary
+    prd_medium_elongation = ((dataframe['height'] / dataframe['width']) > elongation_ratio_medium) & (
+        (dataframe['height'] / dataframe['width']) <= elongation_ratio_high) & prd_bb_on_boundary
+    prd_low_elongation = np.logical_not(
+        (prd_high_elongation | prd_medium_elongation)) & prd_bb_on_boundary
+    prd_inside = np.logical_not(prd_bb_on_boundary)
+
+    high_elongation = (gt_exists & gt_high_elongation) | (
+        np.logical_not(gt_exists) & prd_high_elongation)
+    medium_elongation = (gt_exists & gt_medium_elongation) | (
+        np.logical_not(gt_exists) & prd_medium_elongation)
+    low_elongation = (gt_exists & gt_low_elongation) | (
+        np.logical_not(gt_exists) & prd_low_elongation)
+    inside = (gt_exists & gt_inside) | (np.logical_not(gt_exists) & prd_inside)
+
+    boundary = {'possible partitions': ['inside', 'low_elongation_on_boundary', 'medium_elongation_on_boundary',
+                                        'high_elongation_on_boundary'], 'masks': [inside, low_elongation, medium_elongation, high_elongation]}
+
+    desired_masks_boundary = {'boundary': boundary}
+
+    desired_masks.update(desired_masks_boundary)
 
 
     # masking by bbox meta
@@ -158,10 +228,9 @@ def size_horizontal_vertical_and_bbox_meta(dataframe, from_file=False, img_width
         body_inframe_Other[body_inframe=='MidBody'] = False
         body_inframe_Other[body_inframe=='SideLowerBody'] = False
 
-        body_inframe_dict = {'possible partitions': ['SideBody','FullBody','UpperBoddy','SideUpperBody','LowerBody','MidBody','SideLowerBody','Other_inframe'], 'masks': [body_inframe_SideBody,body_inframe_FullBody,body_inframe_UpperBoddy, body_inframe_SideUpperBody, body_inframe_LowerBody, body_inframe_MidBody, body_inframe_SideLowerBody, body_inframe_Other]}
+        body_inframe_dict = {'possible partitions': ['FullBody','UpperBoddy','SideBody','MidBody','LowerBody','SideUpperBody','SideLowerBody','Other_inframe'], 'masks': [body_inframe_FullBody,body_inframe_UpperBoddy, body_inframe_SideBody, body_inframe_MidBody, body_inframe_LowerBody, body_inframe_SideUpperBody, body_inframe_SideLowerBody, body_inframe_Other]}
         desired_masks.update({"Body_Inframe": body_inframe_dict})
 
-    
 
     # making sure there is no same 'possible partitions' for different partitions
     all_the_options = []
