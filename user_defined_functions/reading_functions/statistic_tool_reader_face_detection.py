1. moved functionality from utils_reading to LogsParser and delted the file. 2. Corrected bug of check_log_type in LogsParser. 3. Separated emulation matrix calculation from the log and bb transform. Better to metform the operastion of emulation matrix from log once and not for every line. 4. Updated transform_bb_to_original_frame_size in LogsParse to work with more genral case of emulation matrix. 5. Updated statistic_tool_reader_presence_algo_logs and statistic_tool_reader_face_detection to work with the updated functions.

user_defined_functions/reading_functions/statistic_tool_reader_face_detection.py
@@ -8,28 +8,28 @@ import os, sys
 current_file_directory = os.path.realpath(__file__)
 # adding the statistics_tool folder to path
 sys.path.append(os.path.join(current_file_directory, '..'))
-from utils_reading import transform_bb_size
+from utils.LogsParser import *
 
 def statistic_tool_reader_face_detection(path):
     with open(path,'r') as file:
         lines = file.readlines()
+        header = lines[0]
+        lines = lines[1:]
 
     if len(lines) < 1:
         return None
     
-    if json.loads(lines[0])['header']['type']=='IO':
-        log_field_type = 'FaceDetectionOutput'
-        emulation_matrix_str = json.loads(lines[0])['header']['emulation_matrix']
-        emulation_matrix_str = emulation_matrix_str .replace('[', '').replace(']', '').replace('\n', '').split()
-        emulation_matrix = np.array(emulation_matrix_str, dtype=float).reshape(3, 3)
-    elif json.loads(lines[0])['header']['type'][0]=='gt_log':
-        log_field_type = 'Face BB'
-        emulation_matrix = np.identity(3)
+    emulation_matrix = get_emulation_matrix(json.loads(header))
 
-    line = json.loads(lines[1])
+    log_type = check_log_type(json.loads(header))
+    if log_type == LogTypes.IO_LOG:
+        log_field_type = 'FaceDetectionOutput'
+    elif log_type == LogTypes.GT_LOG:
+        log_field_type = 'Face BB'
+
     records = []
     
-    for line in lines[1:]:
+    for line in lines:
         line = json.loads(line)
         if ('type' in line['keys'] and line['keys']['type'] != log_field_type) or 'objects' not in line['message']:
             continue
@@ -40,7 +40,7 @@ def statistic_tool_reader_face_detection(path):
         for obj in line['message']['objects']:
             if obj["Source"] != "FACE_DETECTION":
                 continue
-            bb = transform_bb_size(obj['BoundingBox'], emulation_matrix)
+            bb = transform_bb_to_original_frame_size(obj['BoundingBox'], emulation_matrix)
             prediction = {'object_id':obj['Id'], 'x':bb['Left'],'y':bb['Top'],'width':bb['Width'],'height':bb['Height']}
             if 'Score' in obj:
                 prediction['Score'] = obj['Score']

utils/LogsParser.py
@@ -11,13 +11,13 @@ class LogTypes(enum.IntEnum):
     GT_LOG = 1
     IO_LOG = 2
 
-def check_log_type(header):
-    log_type=header['header']['type']
-    if log_type == 'ALGO':
+def check_log_type(header): 
+    log_type = header['header']['type']
+    if (log_type == 'ALGO') | (log_type[0] == 'ALGO'):
         return LogTypes.ALGO_LOG
-    if log_type == 'gt_log':
+    if (log_type == 'gt_log') | (log_type[0] == 'gt_log'):
         return LogTypes.GT_LOG
-    if log_type == 'IO':
+    if (log_type == 'IO') | (log_type[0] == 'IO'):
         return LogTypes.IO_LOG
 
 def is_gt_log(header):
@@ -26,17 +26,31 @@ def is_gt_log(header):
 def is_golden_log(header):
     return check_log_type(header) == LogTypes.ALGO_LOG
 
-def transform_bb_to_original_frame_size(bb, header):
+def get_emulation_matrix(header):
     if 'emulated_resolution' in header['header'].keys():
         if header['header']['emulated_resolution']:
             emulation_matrix=string2array(header['header']['emulation_matrix'])
-            width_scale=emulation_matrix[0,0]
-            height_scale=emulation_matrix[1,1]
-            bb['Left'] = bb['Left']*width_scale
-            bb['Top'] = bb['Top']*height_scale
-            bb['Width'] = bb['Width']*width_scale
-            bb['Height'] = bb['Height']*height_scale
-    return bb
+    else:
+        emulation_matrix = np.identity(3)
+    return emulation_matrix
+
+def transform_bb_to_original_frame_size(bb_object, emulation_matrix):
+    '''
+    Transforms the bounding box size according to the emulation matrix
+    :param bb_object: dictionary with the bounding box information, fields: 'Left', 'Top', 'Width', 'Height'
+    :param emulation_matrix: 3x3 matrix
+    :return: transformed bb_object
+    '''
+    # if emulation matrix is a unit matrix, then no transformation is needed
+    if np.array_equal(emulation_matrix, np.identity(3)):
+        return bb_object
+    else:
+        cord = np.array([bb_object['Left'], bb_object['Top'], 1])
+        cord = emulation_matrix.dot(cord)
+        width_hight = np.array([bb_object['Width'], bb_object['Height'], 0])
+        width_hight = emulation_matrix.dot(width_hight)
+        return {'Left':cord[0],'Top':cord[1],'Width':width_hight[0],'Height':width_hight[1]}
+
 
 def string2array(array_string):
     # remove starting and ending brackets and newline charactes