0c9c004fb94c529b0d789cca9ff6e8a4619917ac
hagai cohen <v-hagaicohen@microsoft.com>
2023-06-07 16:03:43 +0300

    identation

--- flask_GUI/constants.py
+++ flask_GUI/constants.py
@@ -1,19 +1,19 @@
 COLOR_GRADIENT_RED_WHITE_BLUE = [\
-'#ff1c1c',
-'#ff3d3d',
-'#ff5d5d',
-'#ff7e7e',
-'#ff9e9e',
-'#ffbebe',
-'#ffdfdf',
-'#ffffff',
-'#E5F0FF', 
-'#CCE7FF', 
-'#99CEFF', 
-'#66C4FF', 
-'#33BBFF',
-'#00B2FF'
-]
+    '#ff1c1c',
+    '#ff3d3d',
+    '#ff5d5d',
+    '#ff7e7e',
+    '#ff9e9e',
+    '#ffbebe',
+    '#ffdfdf',
+    '#ffffff',
+    '#E5F0FF', 
+    '#CCE7FF', 
+    '#99CEFF', 
+    '#66C4FF', 
+    '#33BBFF',
+    '#00B2FF'
+    ]
 
 COLOR_GRADIENT_RED_BLUE = [\
     '#009900', 
